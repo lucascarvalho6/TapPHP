c14831b03e8cfc97fb81362e33a57e2bbdddde57
lucascarvalho6 <lpcarvalhocontatos@gmail.com>
2024-02-23 21:44:20 -0300

    Aula 23-02

---
+++ ford.php
@@ -0,0 +1,5 @@
+<?php 
+    include_once 'carro.php';
+
+    $ford = new carro();
+?>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+    $nome = "lucas";
+    $idade = 40;
+    $lista = ['banana','uva','limao'];
+
+    echo $nome .'<br>';
+    echo $idade .'<br>';
+    echo $lista .'<br>';
+    echo '<br>';
+    var_dump($nome);
+    echo '<br>';
+    var_dump($idade);
+    echo '<br>';
+    var_dump($lista);
+?>
